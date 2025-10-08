84fed24ae4a10d879fd8f55964c7333ee3bea8b3
= <=>
2025-10-07 21:29:58 -0600

    plantillas

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/inicio')
+def inicio():
+    return render_template("inicio.html")
+
+@app.route('/vehiculos')
+def vehiculos():
+    return render_template("vehiculos.html")
+
+@app.route('/animales')
+def animales():
+    return render_template("animales.html")
+
+@app.route('/maravillas')
+def maravillas():
+    return render_template("maravillas.html")
+
+@app.route('/acerca')
+def acerca():
+    return render_template("acerca.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
